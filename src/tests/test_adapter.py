eb3365d98f130d79291164262e3d7749dddb20c3
xiao-kong-long <2745240762@qq.com>
2023-12-02 23:44:31 +0800

    yes

--- src/tests/test_adapter.py
+++ src/tests/test_adapter.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import datetime
 import unittest
+import datetime
 
 from src.casdoor import CasdoorSDK
 from src.casdoor.adapter import Adapter
